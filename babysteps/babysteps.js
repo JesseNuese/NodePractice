58e664809aa50859184f17aad809497cd5d3bcb8
Student <student@Mac-mini-7.local>
2016-10-24 17:54:07 -0600

    Clickity

--- babysteps/babysteps.js
+++ babysteps/babysteps.js
@@ -1,3 +1,24 @@
-var args = process.argv.slice(2);
+// Http Hello World
+// const http = require('http');
+//
+// http.createServer((req, res) => {
+//   res.writeHead(200, {
+//     'Content-type' : 'text/plain'
+//   });
+//   res.end('Hello, World!\n');
+// }).listen(1337);
 
-console.log(Number(args));
+
+// Express
+// var express = require ('express');
+// var app = express();
+//
+// app.get('/', function (req,res){
+//     res.send('Hello, World!');
+// });
+//
+// app.set('port', (process.env.PORT || 3000));
+//
+// app.listen(app.get('port'), function (){
+//   console.log('Example app listening on port 3000!');
+// });
--- skilljar.js
+++ skilljar.js
@@ -1,3 +1,28 @@
-console.log(process.argv);
+//Baby Steps
 
-for (var i = 2; i < process.argv.length; i ++);
+// var args = process.argv.slice(2);
+// var sum = args.reduce(function(acc, curr){
+//   return +acc + +curr;
+// });
+//
+// console.log(sum);
+
+// My First I/O
+
+// var fs = require('fs');
+//
+// var contents = fs.readFileSync(process.argv[2]);
+//
+// var lines = contents.toString().split('\n').length - 1;
+//
+// console.log(lines);
+
+
+// My First Asynchronous IO
+
+var fs = require('fs');
+
+fs.readFile(process.argv[2], function(err, contents){
+var lines = contents.toString().split('\n').length - 1;
+console.log(lines);
+})
