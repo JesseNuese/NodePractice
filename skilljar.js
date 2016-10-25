b4c2b4b55b7ea29e2322f5a764a4bf9677df18fb
Student <jessenuese@gmail.com>
2016-10-25 11:44:58 -0600

    finished Filtered List

--- skilljar.js
+++ skilljar.js
@@ -30,5 +30,13 @@
 // Filtered List
 
 var fs = require('fs');
+var path = require('path');
 
-fs.readFile(process.argv[2])
+
+fs.readdir(process.argv[2], function(err, list){
+  list.forEach(function (file){
+    if (path.extname(file) === '.' + process.argv[3])
+    console.log(file)
+  });
+
+});
